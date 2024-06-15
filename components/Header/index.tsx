c24d670ad455a947344f03d72ce599247ae0af32
arshad <arshzmuhammed1@gmail.com>
2024-06-15 13:55:11 -0700

    changes

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -30,11 +30,13 @@ const Header = () => {
 
   return (
     <header
-      className={`fixed left-0 top-0 z-99999 w-full ${
+      className={`fixed left-0 top-0 z-99999  ${
         stickyMenu
           ? "bg-white !py-4 shadow transition duration-100 dark:bg-black"
           : ""
-      }`}
+      }`} style={{
+        width:'93%'
+      }}
     >
       <div className="relative mx-auto max-w-c-1390 items-center justify-between px-4 md:px-8 xl:flex 2xl:px-0">
         <div className="flex w-full items-center justify-between xl:w-1/4">
