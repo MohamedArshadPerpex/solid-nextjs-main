9c91792ec9bbad95d571c6fec8054e73a69eefd2
MohamedArshadPerpex <arshad.perpex@gmail.com>
2024-07-01 05:01:55 -0700

    changes

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
     // Simulate loading completion after 3 seconds (Adjust timing as needed)
     const loadingTimeout = setTimeout(() => {
       setLoadingComplete(true);
-    }, 4000);
+    }, 400000);
 
     // Clean up timeout on component unmount or when loading completes early
     return () => clearTimeout(loadingTimeout);
